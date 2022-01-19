5a22fc21eeb1497585c5e7348c6a1b36e4cea759
mrNorii <ksabiron03@gmail.com>
2022-01-19 10:47:10 +0100

    html, css final version, 1st function js + "plus"

---
+++ script.js
@@ -0,0 +1,6 @@
+function dodawanie(){
+    var liczba1=parseInt(document.getElementById("liczba1").value);
+    var liczba2=parseInt(document.getElementById("liczba2").value);
+    var wynik1=liczba1+liczba2;
+    document.getElementById("wynik").innerHTML=wynik1;
+ }
