9f717c51a4554c8cf1d0b2fb520ab8657ab57a57
mrNorii <ksabiron03@gmail.com>
2022-01-19 10:55:09 +0100

    function JS multiplication of numbers

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function dodawanie(){
     var wynik = liczba1-liczba2;
     document.getElementById("wynik").innerHTML=wynik;
  }
+
+ //multiplication of numbers
+ function odejmowanie(){
+    var liczba1 = parseInt(document.getElementById("liczba1").value);
+    var liczba2 = parseInt(document.getElementById("liczba2").value);
+    var wynik = liczba1*liczba2;
+    document.getElementById("wynik").innerHTML=wynik;
+ }
